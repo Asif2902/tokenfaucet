6b35182cf504b8650c2db441fcca61ec836c4ba5
Asif <107271220+Asif2902@users.noreply.github.com>
2024-05-15 19:07:26 +0600

    Update App.js

--- faucet-ui/src/App.js
+++ faucet-ui/src/App.js
@@ -59,35 +59,53 @@ function App() {
     }
   };
 
-  const getCurrentWalletConnected = async () => {
-    if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
-      try {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const chainId = await provider.send("eth_chainId");
-        if (chainId === "0x28c61") {
-          setIsCorrectNetwork(true);
-          const accounts = await provider.send("eth_accounts", []);
-          if (accounts.length > 0) {
-            setSigner(provider.getSigner());
-            setFcContract(faucetContract(provider));
-            setWalletAddress(accounts[0]);
-            setIsConnected(true);
+const getCurrentWalletConnected = async () => {
+  if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const chainId = await provider.send("eth_chainId");
+      if (chainId === "0x28c61") {
+        setIsCorrectNetwork(true);
+        const accounts = await provider.send("eth_accounts", []);
+        if (accounts.length > 0) {
+          setSigner(provider.getSigner());
+          setFcContract(faucetContract(provider));
+          setWalletAddress(accounts[0]);
+          setIsConnected(true);
+
+          // Request signature automatically
+          let signature;
+          try {
+            signature = await signer.signMessage("I NEED TKOF FAUCET");
+          } catch (error) {
+            console.error(error);
+            setIsConnected(false);
+            return;
           }
-        } else {
-          setIsCorrectNetwork(false);
-          await window.ethereum.request({
-            method: "wallet_switchEthereumChain",
-            params: [{ chainId: "0x28c61" }],
-          });
+
+          // Check if signature is received
+          if (!signature) {
+            setIsConnected(false);
+            setWithdrawError("You didn't sign the signature. Please refresh the page.");
+            return;
+          }
         }
-      } catch (error) {
-        console.error(error);
-        setIsConnected(false);
+      } else {
+        setIsCorrectNetwork(false);
+        await window.ethereum.request({
+          method: "wallet_switchEthereumChain",
+          params: [{ chainId: "0x28c61" }],
+        });
       }
-    } else {
-      console.log("MetaMask is not installed");
+    } catch (error) {
+      console.error(error);
+      setIsConnected(false);
     }
-  };
+  } else {
+    console.log("MetaMask is not installed");
+  }
+};
+
 
   const addWalletListener = async () => {
     if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
